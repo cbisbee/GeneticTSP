6987f8faf0274b6aac1187c4e7a470af670be09e
Charles Bisbee <bisbeecharlesl@gmail.com>
2016-11-29 15:24:44 -0700

    Starting to rewrite evolution process, added algorithm steps.

--- GeneticTSP/GeneticTSP/GeneticTSPSource.cpp
+++ GeneticTSP/GeneticTSP/GeneticTSPSource.cpp
@@ -20,18 +20,75 @@ void readData(std::map<int, City> &cityList)
 	}
 }
 
-Individual reproduce(Individual indA, Individual indB, std::map<int,City> mapData)
+//Individual crossover()
+//
+//Individual reproduce(Individual indA, Individual indB, std::map<int,City> mapData)
+//{
+//	Individual newInd = Individual();
+//	//put the first 5 of chromA into newChrom, put the second 5 of chromB into newChrom...
+//	for (int i = 0; i < TOURSIZE; i++) {
+//		/*
+//		if ((i < 5) || (i >= 10 && i < 15) || (i >= 20))
+//			newInd.addCity(indA.getCity(i));
+//		else
+//			newInd.addCity(indB.getCity(i));
+//			*/
+//		if ((i <= ((int)TOURSIZE / 2) || i == (TOURSIZE - 1)))
+//			newInd.addCity(indA.getCity(i));
+//		else
+//			newInd.addCity(indB.getCity(i));
+//	}
+//	newInd.setFitness(mapData);
+//	return newInd;
+//}
+
+Individual rouletteWheel(Population pop)
 {
-	Individual newInd = Individual();
-	//put the first 5 of chromA into newChrom, put the second 5 of chromB into newChrom...
-	for (int i = 0; i < TOURSIZE; i++) {
-		if ((i < 5) || (i >= 10 && i < 15) || (i >= 20))
-			newInd.addCity(indA.getCity(i));
-		else
-			newInd.addCity(indB.getCity(i));
-	}
-	newInd.setFitness(mapData);
-	return newInd;
+
+}
+
+void mutate(Individual &curInd, std::map<int, City> mapData)
+{
+	//loop through individual
+		//create a random number and check if it is equal to some CONSTANT MUTATION RATE
+			//get a random position with the bounds of the individual
+			//swap the cities at i and at the random pos you found
+}
+
+Individual crossover(Individual p1, Individual p2, std::map<int, City> mapData)
+{
+	//use roulette wheel or tournament selection to get p1 and p2
+
+	int startPos = rand() % TOURSIZE; //lower bound of p1 subtour
+	int endPos = rand() % TOURSIZE; //upper bound of p1 subtour
+
+	Individual child; //creating new child
+
+	//loop and add the subtour of p1 to child
+
+
+	//loop and add the cities from p2 that aren't already in child into any remaining spots in child until that bad boy is full
+
+	//return the new individual child
+
+}
+
+Population evolve(Population pop, std::map<int, City> mapData)
+{
+	//create new population
+
+	//determine whether or not oyu want eletism (NEED A GLOBAL VARIABLE FOR THIS)
+
+
+	//LOOP THIS UNTIL NEW POPULATION HAS TOURSIZE AMOUNT OF INDIVIDUALS
+		//create two Individuals (p1 and p2) using roulette wheel or tournament selection
+
+		//create a child Individual and set it equal to the crossover of p1 and p2
+
+	//add a bit of mutation to each individual (this involves a loop)
+
+	//return the new population
+
 }
 
 //TODO: Implement actual addition of generations in main, need to figure that logic out.
